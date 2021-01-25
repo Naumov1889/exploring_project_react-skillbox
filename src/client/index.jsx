d9f2d7c36fa887e09ceb48377b26263e9eaacbb0
Naumov1889 <maksim.naumov1889@gmail.com>
2021-01-25 21:23:22 +0500

    entry component

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import * as ReactDOM from 'react-dom';
-import {Header} from "../shared/Header";
+import {App} from '../App';
 import {StarWarsNameClass} from "../shared/StarWarsNameClass.tsx";
 import {StarWarsNameFunction} from "../shared/StarWarsNameFunction.tsx";
 
 // since we don't want header to be rendered via SSR
 window.addEventListener('load', () => {
-    // ReactDOM.hydrate(<Header/>, document.getElementById('react_root'));
-    // ReactDOM.hydrate(<StarWarsNameClass/>, document.getElementById('react_root'));
-    ReactDOM.hydrate(<StarWarsNameFunction/>, document.getElementById('react_root'));
+    ReactDOM.hydrate(<App/>, document.getElementById('react_root'));
+    // ReactDOM.hydrate(<StarWarsNameFunction/>, document.getElementById('react_root'));
 });
