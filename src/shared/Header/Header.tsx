ada0b8e22de882565f10dfaf2d491d3a8a9848ae
Naumov1889 <maksim.naumov1889@gmail.com>
2021-02-25 23:10:38 +0500

    react context 2

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,19 +1,13 @@
-import React, {useContext} from 'react';
+import React from 'react';
 import styles from './header.css';
 import {SortBlock} from "./SortBlock";
 import {SearchBlock} from "./SearchBlock";
 import {ThreadTitle} from "./ThreadTitle";
-import {tokenContext} from "../context/tokenContext";
 
 export function Header() {
-    // const {Consumer} = tokenContext;
-    const token = useContext(tokenContext);
     return (
         <header className={styles.header}>
-            {/*<Consumer>*/}
-            {/*    {(token) => }*/}
-            {/*</Consumer>*/}
-            <SearchBlock token={token}/>
+            <SearchBlock/>
             <ThreadTitle/>
             <SortBlock/>
         </header>
