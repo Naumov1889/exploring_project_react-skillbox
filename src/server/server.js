03d3f25a3d76f4ea71d3d1df1543f933c788bee3
Naumov1889 <maksim.naumov1889@gmail.com>
2021-03-27 19:07:59 +0500

    URI var rename, mb this solves env var conflict

--- src/server/server.js
+++ src/server/server.js
@@ -23,7 +23,7 @@ app.use('/static', express.static('./dist/client'))
 app.get('/auth/', (req, res) => {
     axios.post(
         'https://www.reddit.com/api/v1/access_token',
-        `grant_type=authorization_code&code=${req.query.code}&redirect_uri=${process.env.URI}/auth/`,
+        `grant_type=authorization_code&code=${req.query.code}&redirect_uri=${process.env.MYNONCONLICTVARNAME}/auth/`,
         {
             auth: {username: process.env.CLIENT_ID, password: process.env.SECRET},
             headers: {'Content-type': 'application/x-www-form-urlencoded'}
@@ -45,5 +45,5 @@ app.get('*', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server started on ${process.env.URI}:${PORT}`);
+    console.log(`Server started on ${process.env.MYNONCONLICTVARNAME}:${PORT}`);
 })
